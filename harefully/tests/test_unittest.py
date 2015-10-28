622770ec0f6cf3c2e77684231242ec7a8e9ce6b6
Jack <jack.grahl@yahoo.co.uk>
2015-10-28 22:58:29 +0000

    Give this test a reasonable name.

--- harefully/tests/test_unittest.py
+++ harefully/tests/test_unittest.py
@@ -4,7 +4,7 @@ from harefully import test_case
 
 class TestUnittest(unittest.TestCase):
     """ Tests the function which makes a unittest test case from a class."""
-    def test_test_case(self):
+    def test_creates_a_subclass_of_unittest__TestCase(self):
         a = object
         tc = test_case(a)
         self.assertTrue(issubclass(tc, unittest.TestCase))
