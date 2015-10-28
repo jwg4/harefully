fe2ec9d9a325bdce681229414c3d90dbfd1cd471
Jack <jack.grahl@yahoo.co.uk>
2015-10-28 23:16:10 +0000

    Factor out creating a type variable.

--- harefully/tests/test_unittest.py
+++ harefully/tests/test_unittest.py
@@ -6,20 +6,21 @@ from harefully import test_case
 
 class TestUnittest(unittest.TestCase):
     """ Tests the function which makes a unittest test case from a class."""
+    def setUp(self):
+        class a(object):
+            pass
+        self.cl_type = a
+        
     def test_creates_a_subclass_of_unittest__TestCase(self):
         """ Check that the decorated class is a subclass of unittest.TestCase._"""
-        a = object
-        tc = test_case(a)
+        setattr(self.cl_type, 'tests', [1])
+        tc = test_case(self.cl_type)
         self.assertTrue(issubclass(tc, unittest.TestCase))
 
     def test_creates_a_test_function(self):
         """ Check that we get at least one function whose name starts with 'test_' in the decorated class."""
-        class a(object):
-            pass
-        setattr(a, 'tests', [1])
-
-        tc = test_case(a)
+        setattr(self.cl_type, 'tests', [1])
+        tc = test_case(self.cl_type)
         methods = inspect.getmembers(tc, predicate=inspect.ismethod)
         test_methods = [ m for m in methods if m[0].startswith('test_') ]
         self.assertTrue(len(test_methods) > 0, sorted([ m[0] for m in methods ]) )
-
